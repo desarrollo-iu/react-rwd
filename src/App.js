e9a3cb2a4fe9ccf11bc4c0a80ee62069c433780f
Carlos Vega Meyer <carlosvega09@gmail.com>
2017-05-10 19:46:11 -0300

    Adds ResponsiveColumn component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ResponsiveColumn from './components/ResponsiveColumn';
+
+const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 class App extends Component {
   render() {
@@ -10,9 +13,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ResponsiveColumn>
+          {LOREM_IPSUM.repeat(10)}
+        </ResponsiveColumn>
+        <ResponsiveColumn>
+          {LOREM_IPSUM.repeat(10)}
+        </ResponsiveColumn>
       </div>
     );
   }
---
+++ src/components/ResponsiveColumn/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './styles.css'
+
+export default ({ children }) => <p className="ResponsiveColumn-text">{children}</p>
